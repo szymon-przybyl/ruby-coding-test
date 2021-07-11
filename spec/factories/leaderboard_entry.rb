499bfc827ffef56050afd123b86b95aae390f4ee
Szymon Przybył <szymon.przybyl7@gmail.com>
2021-07-11 15:56:56 +0200

    Add specs for LeaderboardEntryScore

--- spec/factories/leaderboard_entry.rb
+++ spec/factories/leaderboard_entry.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :leaderboard_entry do
     username { 'John' }
     score { 0 }
+    leaderboard
   end
 end
--- spec/models/leaderboard_entry_score_spec.rb
+++ spec/models/leaderboard_entry_score_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe LeaderboardEntryScore, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:leaderboard_entry) { create(:leaderboard_entry, score: 50) }
+
+  it 'updates entry score upon creation' do
+    leaderboard_entry.scores.create score: 10
+    expect(leaderboard_entry.reload.score).to eq 60
+  end
+
+  it 'updates entry score upon deletion' do
+    entry = leaderboard_entry.scores.create score: 10
+    entry.destroy
+    expect(leaderboard_entry.reload.score).to eq 50
+  end
 end
