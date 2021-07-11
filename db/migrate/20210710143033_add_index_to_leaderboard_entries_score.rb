5fbdd2a1178cd54f265002a931ddf72160e08cbb
Szymon Przybył <szymon.przybyl7@gmail.com>
2021-07-11 22:04:43 +0200

    Set descending order for entries scores index

--- db/migrate/20210710143033_add_index_to_leaderboard_entries_score.rb
+++ db/migrate/20210710143033_add_index_to_leaderboard_entries_score.rb
@@ -1,5 +1,5 @@
 class AddIndexToLeaderboardEntriesScore < ActiveRecord::Migration[5.1]
   def change
-    add_index :leaderboard_entries, [:leaderboard_id, :score]
+    add_index :leaderboard_entries, [:leaderboard_id, :score], order: { score: :desc }
   end
 end
