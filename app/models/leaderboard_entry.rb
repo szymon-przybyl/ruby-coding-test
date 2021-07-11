ed12db59fccc1b8eaa4cdcc0cb1de55ec6f3dfae
Szymon Przybył <szymon.przybyl7@gmail.com>
2021-07-11 17:16:19 +0200

    Move ordering of entries into scope

--- app/models/leaderboard_entry.rb
+++ app/models/leaderboard_entry.rb
@@ -2,7 +2,9 @@ class LeaderboardEntry < ApplicationRecord
   belongs_to :leaderboard
   has_many :scores, class_name: 'LeaderboardEntryScore', dependent: :destroy
 
+  scope :by_score, -> { order(score: :desc) }
+
   def position
-    leaderboard.entries.order(score: :desc).pluck(:id).index(id) + 1
+    leaderboard.entries.by_score.pluck(:id).index(id) + 1
   end
 end
